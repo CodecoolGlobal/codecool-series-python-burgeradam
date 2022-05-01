ced5e7ccf1754aab9b02ec14b64bfb91fb3f363a
Burger Adam <burger.adam89@gmail.com>
2022-05-01 15:13:15 +0200

    initialized task with base settings

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,9 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def search(usr_input):
+    return data_manager.execute_select('''
+                                        SELECT *
+                                        FROM shows''')
